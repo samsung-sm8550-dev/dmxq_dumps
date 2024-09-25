ac55760e9cb9093f6fe27361789487a58b16b8f9
chaptsand <chaptsand@gmail.com>
2024-09-25 18:10:34 +0800

    S916BXXS6CXHA init

--- rootdir/bin/install-recovery.sh
+++ rootdir/bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/vendor/bin/sh
-if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):109051904:7b491e97e6b7bf7c9d97264eab9dccfeb85fbcdb; then
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):109051904:d82520ada10d12e548dd67f7315f8f29043c5b46; then
   applypatch \
           --patch /vendor/recovery-from-boot.p \
-          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):100663296:9cb51ecf2034e08108dcac03a71454f98dae6ab9 \
-          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):109051904:7b491e97e6b7bf7c9d97264eab9dccfeb85fbcdb && \
+          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):100663296:df4eac7bc3e75c1f3731f51a2b8ccc2a8cfe9fd6 \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):109051904:d82520ada10d12e548dd67f7315f8f29043c5b46 && \
       (log -t install_recovery "Installing new recovery image: succeeded" && setprop vendor.ota.recovery.status 200) || \
       (log -t install_recovery "Installing new recovery image: failed" && setprop vendor.ota.recovery.status 454)
 else
